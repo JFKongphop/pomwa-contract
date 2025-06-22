873146c583b906ff7ce8e55a7c7265372704d43b
JFKongphop <kongphopleo@gmail.com>
2025-06-22 22:38:33 +0700

    add utils for test

--- test/test.ts
+++ test/test.ts
@@ -1,23 +1,22 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
-// import { } from '../typechain-types';
+import { Groth16Verifier } from '../typechain-types';
 
 describe('Contract', async () => {
   let user1: SignerWithAddress; 
   let user2: SignerWithAddress;
-  let contract: any;
-
+  let contract: Groth16Verifier;
 
   before(async () => {
     [user1, user2] = await ethers.getSigners();
     
-    const Contract = await ethers.getContractFactory('');
+    const Contract = await ethers.getContractFactory('Groth16Verifier');
     contract = await Contract.deploy();
   });
 
-  describe('Function', async () => {
-    it('Should return', async () => {
+  describe('verify', async () => {
+    it('Should return true', async () => {
 
     });
   })
