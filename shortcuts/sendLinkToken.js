eabd96c8625e88c4afe07ba5a4cd083b35fcc806
JFKongphop <kongphopleo@gmail.com>
2025-06-23 00:30:49 +0700

    completed deposit and withdraw basic

--- shortcuts/sendLinkToken.js
+++ shortcuts/sendLinkToken.js
@@ -5,21 +5,29 @@ dotenv.config();
 const providerURL = process.env.SEPOLIA
 const privateKey = process.env.PRIVATE_KEY;
 const depositAddress = process.env.DEPOSIT;
+const linkAbi = [
+  {
+    constant: false,
+    inputs: [
+      { name: "_to", type: "address" },
+      { name: "_value", type: "uint256" },
+      { name: "_data", type: "bytes" }
+    ],
+    name: "transferAndCall",
+    outputs: [{ name: "success", type: "bool" }],
+    type: "function"
+  }
+];
 
 const execution = async () => {  
   const provider = new ethers.JsonRpcProvider(providerURL);
   const wallet = new ethers.Wallet(privateKey, provider);
   const link = new ethers.Contract(
     '0x779877A7B0D9E8603169DdbD7836e478b4624789',
-    [
-      'function approve(address spender, uint256 amount) public returns (bool)',
-      'function allowance(address owner, address spender) public view returns (uint256)',
-      'function balanceOf(address account) public view returns (uint256)',
-      'function transfer(address to, uint256 amount) public returns (bool)',
-    ],
+    linkAbi,
     wallet
   );
-
+  
   const tx = await link.transferAndCall(depositAddress, ethers.parseEther('1'), '0x');
 
   const receipt = await tx.wait();
